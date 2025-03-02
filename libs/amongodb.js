c4d7b83cd37efdc8b49a6120d906987d5064fe74
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2025-03-02 22:06:29 +0400

    auth

--- libs/amongodb.js
+++ libs/amongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const aconnectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.AMONGODB_URI);
+    await mongoose.connect(process.env.AUTH_MONGODB_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Error connecting to MongoDB: ", error);
