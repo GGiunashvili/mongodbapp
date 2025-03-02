c3ac6df16d6459771f06c86c9c1bed825935aa01
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2025-03-02 16:01:00 +0400

    header

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,8 +4,15 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between items-center bg-slate-800 px-8 py-3 mb-[20px]">
       <Link className="text-white font-bold" href={"/"}>
-        GTCoding.
+        register
       </Link>
+      <Link className="text-white font-bold" href={"/dashboard"}>
+        profile
+      </Link>
+      <Link className="text-white font-bold" href={"/admin"}>
+        admin
+      </Link>
+
       <Link className="bg-white p-2" href={"/addTopic"}>
         Add Topic
       </Link>
